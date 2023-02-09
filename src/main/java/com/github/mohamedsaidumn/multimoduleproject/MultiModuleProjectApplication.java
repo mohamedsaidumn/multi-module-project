341e8bd85e6bedac3c1874907fcb73ed40c2f17a
mohamedsaid321 <mohamedsaidumn@gmail.com>
2023-02-09 12:20:23 -0600

    initialize multi module project example for spring

---
+++ src/main/java/com/github/mohamedsaidumn/multimoduleproject/MultiModuleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.github.mohamedsaidumn.multimoduleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiModuleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiModuleProjectApplication.class, args);
+	}
+
+}
